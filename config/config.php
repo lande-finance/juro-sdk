66d8329410f574b412a97d4d86b98c6549a556e5
AlexN <aleksandrs.nastasins@lande.finance>
2023-04-28 13:39:07 +0300

    config key fix

--- config/config.php
+++ config/config.php
@@ -4,6 +4,6 @@
  * You can place your Juro SDK configuration in here.
  */
 return [
-    'api-key' => env('JURO_API_KEY'),
+    'api_key' => env('JURO_API_KEY'),
     'debug' => env('JURO_DEBUG', false),
 ];
